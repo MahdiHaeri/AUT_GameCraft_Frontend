f47f5652e4f9b38d61d5cb8b8a7300f14ea52138
Mahdi Haeri <mahdihaerim@gmail.com>
2024-08-14 03:22:04 +0330

    Add Font

--- src/config/customTheme.js
+++ src/config/customTheme.js
@@ -1,8 +1,13 @@
 import {theme} from "antd";
+import '/src/assets/fonts/SF_Pro_Rounded/sfProRounded.css'
+import '/src/assets/fonts/IRANYekanX/fontiran.css'
+import '/src/assets/fonts/Estedad-v7.3/estedad.css'
+import '/src/assets/fonts/vazirmatn-v33.003/Vazirmatn-font-face.css'
 
 const customTheme = {
     algorithm: theme.defaultAlgorithm,
     token: {
+        fontFamily: "SF-Pro-Rounded",
         borderRadius: 12,
         colorPrimary: "#3c3a7d",
         primaryColor: "#3c3a7d",
