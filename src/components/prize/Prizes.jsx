8d7fc545c15628f39db5a319cf51592d56f31b83
Mahdi Haeri <mahdihaerim@gmail.com>
2024-08-16 15:35:59 +0330

    Fix bug in Prizes

--- src/components/prize/Prizes.jsx
+++ src/components/prize/Prizes.jsx
@@ -7,24 +7,24 @@ export function Prizes() {
     return (
         <Flex vertical align={"center"} justify={"center"} style={{width: '100%'}}>
             <Typography.Title level={1} style={{fontWeight: 'bolder'}}>Contest prizes</Typography.Title>
-            <Row align={"middle"} justify={"center"} gutter={[16, 16]}>
+            <Row align={"middle"} justify={"center"} style={{width: '100%'}} gutter={[16, 16]}>
                 <Col span={24} lg={12}>
                     <Flex vertical align={"center"} justify={"center"} style={{width: '100%'}} gap={"small"}>
-                        <img src={firstPrizeLogo} alt={'first-prize-logo'} width={'50%'} height={'auto'}/>
+                        <img src={firstPrizeLogo} alt={'first-prize-logo'} width={'40%'} height={'auto'}/>
                         <Typography.Title level={2} style={{margin: 0}}>Team 1</Typography.Title>
                         <Typography.Title level={4} style={{margin: 0}}>9 Million Toman</Typography.Title>
                     </Flex>
                 </Col>
                 <Col span={24} lg={12}>
                     <Flex vertical align={"center"} justify={"center"} style={{width: '100%'}} gap={"small"}>
-                        <img src={secondPrizeLogo} alt={'second-prize-logo'} width={'50%'} height={'auto'}/>
+                        <img src={secondPrizeLogo} alt={'second-prize-logo'} width={'40%'} height={'auto'}/>
                         <Typography.Title level={2} style={{margin: 0}}>Team 2</Typography.Title>
                         <Typography.Title level={4} style={{margin: 0}}>6 Million Toman</Typography.Title>
                     </Flex>
                 </Col>
                 <Col span={24} lg={12}>
                     <Flex vertical align={"center"} justify={"center"} style={{width: '100%'}} gap={"small"}>
-                        <img src={thirdPrizeLogo} alt={'third-prize-logo'} width={'50%'} height={'auto'}/>
+                        <img src={thirdPrizeLogo} alt={'third-prize-logo'} width={'40%'} height={'auto'}/>
                         <Typography.Title level={2} style={{margin: 0}}>Team 3</Typography.Title>
                         <Typography.Title level={4} style={{margin: 0}}>3 Million Toman</Typography.Title>
                     </Flex>
