99ddef3e8e368890d0a8a0f67a568ae79899f639
Mahdi Haeri <mahdihaerim@gmail.com>
2024-08-16 05:06:12 +0330

    Update FAQ

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -1,5 +1,4 @@
-import {Col, Flex, Row, theme, Typography} from "antd";
-import {StaffCard} from "../../view/StaffView/component/StaffCard.jsx";
+import {Flex, theme, Typography} from "antd";
 
 const {useToken} = theme
 
@@ -41,6 +40,13 @@ export function Faq() {
 
                 <Typography.Title level={3}>
                     judging criteria and prizes
+                </Typography.Title>
+                <Typography.Text>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi at commodi debitis eaque error, et exercitationem fugit harum impedit incidunt ipsam itaque minus neque, optio perspiciatis praesentium quasi rerum saepe sunt ullam veniam voluptas! Accusamus assumenda atque autem commodi cupiditate dolores, ex impedit ipsum itaque non, officiis repellat velit.
+                </Typography.Text>
+
+                <Typography.Title level={3}>
+                    The deadline for submitting works
                 </Typography.Title>
                 <Typography.Text>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi at commodi debitis eaque error, et exercitationem fugit harum impedit incidunt ipsam itaque minus neque, optio perspiciatis praesentium quasi rerum saepe sunt ullam veniam voluptas! Accusamus assumenda atque autem commodi cupiditate dolores, ex impedit ipsum itaque non, officiis repellat velit.
--- src/view/FaqView/FaqView.jsx
+++ src/view/FaqView/FaqView.jsx
@@ -1,9 +1,5 @@
 import {Flex, theme} from "antd";
 import backgroundPattern from "../../assets/svg/pattern.svg";
-import {GameCraftIntro} from "../../components/introduction/GameCraftIntro.jsx";
-import {AboutUs} from "../../components/aboutUs/AboutUs.jsx";
-import {Prizes} from "../../components/prize/Prizes.jsx";
-import {Sponsors} from "../../components/sponsor/Sponsors.jsx";
 import {Faq} from "../../components/Faq/Faq.jsx";
 
 const {useToken} = theme
