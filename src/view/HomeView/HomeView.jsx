91fad4efc61d634b9d96da05568fceee860b0c80
Mahdi Haeri <mahdihaerim@gmail.com>
2024-08-19 12:57:06 +0330

    Add Wave component

--- src/view/HomeView/HomeView.jsx
+++ src/view/HomeView/HomeView.jsx
@@ -1,14 +1,10 @@
 import {Flex, Grid, theme} from "antd";
-import backgroundPattern from "../../assets/svg/pattern.svg";
 import {Sponsors} from "../../components/sponsor/Sponsors.jsx";
 import {GameCraftIntro} from "../../components/introduction/GameCraftIntro.jsx";
 import {AboutUs} from "../../components/aboutUs/AboutUs.jsx";
 import {Prizes} from "../../components/prize/Prizes.jsx";
 import {GameCraftTimeline} from "../../components/Timeline/GameCraftTimeline.jsx";
-import wavePurple from '/src/assets/svg/wave-purple.svg'
-import wavePurpleLite from '/src/assets/svg/wave-purple-light.svg'
-import waveGreen from '/src/assets/svg/wave-green.svg'
-import router from "../../routes/index.jsx";
+import {Wave} from "../../components/wave/Wave.jsx";
 
 const {useToken} = theme
 const {useBreakpoint} = Grid;
@@ -17,6 +13,7 @@ export function HomeView() {
     const {token} = useToken()
     const screens = useBreakpoint();
     const homeViewPadding = screens.lg ? '0 5rem' : '0 3rem'
+    // padding: screens.xl ? '3rem 8rem' : screens.lg ? '3rem 6rem' : screens.md ? '3rem 4rem' : '3rem 2rem',
 
     return (
         <Flex
@@ -25,33 +22,18 @@ export function HomeView() {
             vertical
             style={{
                 width: '100%',
-                backgroundColor: 'white',
-                // backgroundColor: token.colorPrimary,
-                // backgroundImage: `url(${backgroundPattern})`,
-                // padding: screens.xl ? '3rem 8rem' : screens.lg ? '3rem 6rem' : screens.md ? '3rem 4rem' : '3rem 2rem',
+                backgroundColor: token.colorBgBase,
             }}
         >
             <GameCraftIntro padding={homeViewPadding} backgroundColor={token.colorPrimary}/>
-            <img src={wavePurple} alt={'wave-purple'} width={'100%'} height={'auto'}/>
-            <GameCraftTimeline padding={homeViewPadding} backgroundColor={'white'}/>
-            <Prizes padding={homeViewPadding} backgroundColor={'white'}/>
-            <img
-                src={wavePurple}
-                alt={'wave-purple'}
-                width={'100%'}
-                height={'auto'}
-                style={{transform: 'scaleY(-1) translateY(-2px)'}}
-            />
+            <Wave width={'100%'} height={'auto'} fill={token.colorPrimary}/>
+            <GameCraftTimeline padding={homeViewPadding} backgroundColor={token.colorBgBase}/>
+            <Prizes padding={homeViewPadding} backgroundColor={token.colorBgBase}/>
+            <Wave width={'100%'} height={'auto'} fill={token.colorPrimary} style={{transform: 'scaleY(-1) translateY(-2px)'}}/>
             <Sponsors padding={homeViewPadding} backgroundColor={token.colorPrimary}/>
-            <img src={wavePurple} alt={'wave-purple'} width={'100%'} height={'auto'}/>
-            <AboutUs padding={homeViewPadding} backgroundColor={'white'}/>
-            <img
-                src={wavePurple}
-                alt={'wave-purple'}
-                width={'100%'}
-                height={'auto'}
-                style={{transform: 'scaleY(-1) translateY(-2px)'}}
-            />
+            <Wave width={'100%'} height={'auto'} fill={token.colorPrimary}/>
+            <AboutUs padding={homeViewPadding} backgroundColor={token.colorBgBase}/>
+            <Wave width={'100%'} height={'auto'} fill={token.colorPrimary} style={{transform: 'scaleY(-1) translateY(-2px)'}}/>
         </Flex>
     )
 }
