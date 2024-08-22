2d5121c1a023d33c735c185f01eb9b17afb201a4
Mahdi Haeri <mahdihaerim@gmail.com>
2024-08-22 16:59:21 +0330

    Update footer to be more responsive

--- src/components/footer/AppFooter.jsx
+++ src/components/footer/AppFooter.jsx
@@ -1,4 +1,4 @@
-import {Button, Divider, Flex, Layout, theme, Typography} from "antd";
+import {Button, Col, Divider, Flex, Layout, Row, theme, Typography} from "antd";
 import logo from '/src/assets/svg/dark-3d.svg'
 import autComputerEngineeringLogo from '/src/assets/images/logo/ssc_white.png'
 import tehranArtLogo from '/src/assets/images/logo/Asset 4.png'
@@ -30,43 +30,49 @@ export function AppFooter() {
                 {t('app.footer.autGameCraft')}
             </Typography.Title>
             <Flex align={"center"} justify={"space-between"} style={{width: '100%'}}>
-                <Flex
-                    align={"center"}
-                    justify={"center"}
-                    flex={1}
-                    gap={"large"}
-                >
-                    <img
-                        src={logo}
-                        alt={'logo'}
-                        width={'10%'}
-                        height={'auto'}
-                    />
-                    <img
-                        src={autComputerEngineeringLogo}
-                        alt={'aut-computer-engineering-logo'}
-                        width={'10%'}
-                        height={'auto'}
-                    />
-                    <img
-                        src={tehranArtLogo}
-                        alt={'tehran-art-logo'}
-                        width={'10%'}
-                        height={'auto'}
-                    />
-                </Flex>
-                <Flex
-                    align={"center"}
-                    justify={"center"}
-                    flex={1}
-                    gap={"small"}
-                >
-                    <Button type={"text"} shape={"circle"} size={"large"} icon={<YoutubeFilled/>}/>
-                    <Button type={"text"} shape={"circle"} size={"large"} icon={<XOutlined/>}/>
-                    <Button type={"text"} shape={"circle"} size={"large"} icon={<InstagramOutlined/>}/>
-                    <Button type={"text"} shape={"circle"} size={"large"} icon={<FacebookFilled/>}/>
-                    <Button type={"text"} shape={"circle"} size={"large"} icon={<LinkedinFilled/>}/>
-                </Flex>
+                <Row align={"middle"} justify={"space-around"} gutter={[16, 16]}>
+                    <Col span={24} md={12}>
+                        <Flex
+                            align={"center"}
+                            justify={"center"}
+                            flex={1}
+                            gap={"large"}
+                        >
+                            <img
+                                src={logo}
+                                alt={'logo'}
+                                width={'10%'}
+                                height={'auto'}
+                            />
+                            <img
+                                src={autComputerEngineeringLogo}
+                                alt={'aut-computer-engineering-logo'}
+                                width={'10%'}
+                                height={'auto'}
+                            />
+                            <img
+                                src={tehranArtLogo}
+                                alt={'tehran-art-logo'}
+                                width={'10%'}
+                                height={'auto'}
+                            />
+                        </Flex>
+                    </Col>
+                    <Col span={24} md={12}>
+                        <Flex
+                            align={"center"}
+                            justify={"center"}
+                            flex={1}
+                            gap={"small"}
+                        >
+                            <Button type={"text"} shape={"circle"} size={"large"} icon={<YoutubeFilled/>}/>
+                            <Button type={"text"} shape={"circle"} size={"large"} icon={<XOutlined/>}/>
+                            <Button type={"text"} shape={"circle"} size={"large"} icon={<InstagramOutlined/>}/>
+                            <Button type={"text"} shape={"circle"} size={"large"} icon={<FacebookFilled/>}/>
+                            <Button type={"text"} shape={"circle"} size={"large"} icon={<LinkedinFilled/>}/>
+                        </Flex>
+                    </Col>
+                </Row>
             </Flex>
             <Divider>
                 <Typography.Text type={"secondary"}>
