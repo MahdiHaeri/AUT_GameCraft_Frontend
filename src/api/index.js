0f1c9f5a59c1ac641788303a935f39d15fbfd8ed
Mahdi Haeri <mahdihaerim@gmail.com>
2024-08-23 17:38:15 +0330

    Add axios config

---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API = axios.create({
+    baseURL: import.meta.env.VITE_API_URL, // Access the environment variable here
+});
+
+// Request interceptor to add the JWT token conditionally
+API.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token && config.requiresAuth) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => Promise.reject(error));
+
+export default API;
