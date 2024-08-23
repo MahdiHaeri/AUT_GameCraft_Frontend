2ad081161ee8f07c986d093d8f22379fbf98785d
Mahdi Haeri <mahdihaerim@gmail.com>
2024-08-24 02:38:42 +0330

    Add register api

--- src/api/services/auth.js
+++ src/api/services/auth.js
@@ -4,3 +4,8 @@ export const login = async (email, password) => {
     const response = await API.post('/v2/token/', { email, password });
     return response.data;
 }
+
+export const register = async (username, email, phoneNumber, password) => {
+    const response = await API.post('/v2/users/sign_up/', { username, email, phoneNumber, password });
+    return response.data;
+}
