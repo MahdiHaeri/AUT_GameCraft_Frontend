ee9a25bef088d9c83a0da7f09851190e89efc5d5
Mahdi Haeri <mahdihaerim@gmail.com>
2024-09-21 13:47:00 +0330

    Add darkTheme to intro, footer

--- src/components/footer/AppFooter.jsx
+++ src/components/footer/AppFooter.jsx
@@ -1,4 +1,4 @@
-import {Button, Col, Divider, Flex, Layout, Row, theme, Typography} from "antd";
+import {Button, Col, ConfigProvider, Divider, Flex, Layout, Row, theme, Typography} from "antd";
 import logo from '/src/assets/svg/dark-3d.svg'
 import autComputerEngineeringLogo from '/src/assets/images/logo/ssc_white.png'
 import tehranArtLogo from '/src/assets/images/logo/Asset 4.png'
@@ -10,6 +10,7 @@ import {
     XOutlined,
     YoutubeFilled
 } from "@ant-design/icons";
+import {darkTheme} from "../../config/theme/darkTheme.js";
 
 const {useToken} = theme
 
@@ -18,67 +19,69 @@ export function AppFooter() {
     const {t} = useTranslation()
 
     return (
-        <Layout.Footer
-            style={{
-                backgroundColor: token.colorPrimary,
-                textAlign: 'center',
-                width: '100%',
-                padding: token.padding,
-            }}
-        >
-            <Typography.Title level={2}>
-                {t('app.footer.autGameCraft')}
-            </Typography.Title>
-            <Flex align={"center"} justify={"space-between"} style={{width: '100%'}}>
-                <Row align={"middle"} justify={"space-around"} gutter={[16, 16]}>
-                    <Col span={24} md={12}>
-                        <Flex
-                            align={"center"}
-                            justify={"center"}
-                            flex={1}
-                            gap={"large"}
-                        >
-                            <img
-                                src={logo}
-                                alt={'logo'}
-                                width={'10%'}
-                                height={'auto'}
-                            />
-                            <img
-                                src={autComputerEngineeringLogo}
-                                alt={'aut-computer-engineering-logo'}
-                                width={'10%'}
-                                height={'auto'}
-                            />
-                            <img
-                                src={tehranArtLogo}
-                                alt={'tehran-art-logo'}
-                                width={'10%'}
-                                height={'auto'}
-                            />
-                        </Flex>
-                    </Col>
-                    <Col span={24} md={12}>
-                        <Flex
-                            align={"center"}
-                            justify={"center"}
-                            flex={1}
-                            gap={"small"}
-                        >
-                            <Button type={"text"} shape={"circle"} size={"large"} icon={<YoutubeFilled/>}/>
-                            <Button type={"text"} shape={"circle"} size={"large"} icon={<XOutlined/>}/>
-                            <Button type={"text"} shape={"circle"} size={"large"} icon={<InstagramOutlined/>}/>
-                            <Button type={"text"} shape={"circle"} size={"large"} icon={<FacebookFilled/>}/>
-                            <Button type={"text"} shape={"circle"} size={"large"} icon={<LinkedinFilled/>}/>
-                        </Flex>
-                    </Col>
-                </Row>
-            </Flex>
-            <Divider>
-                <Typography.Text type={"secondary"}>
-                    {t('app.footer.copyRight')}
-                </Typography.Text>
-            </Divider>
-        </Layout.Footer>
+        <ConfigProvider theme={darkTheme}>
+            <Layout.Footer
+                style={{
+                    backgroundColor: token.colorPrimary,
+                    textAlign: 'center',
+                    width: '100%',
+                    padding: token.padding,
+                }}
+            >
+                <Typography.Title level={2}>
+                    {t('app.footer.autGameCraft')}
+                </Typography.Title>
+                <Flex align={"center"} justify={"space-between"} style={{width: '100%'}}>
+                    <Row align={"middle"} justify={"space-around"} gutter={[16, 16]}>
+                        <Col span={24} md={12}>
+                            <Flex
+                                align={"center"}
+                                justify={"center"}
+                                flex={1}
+                                gap={"large"}
+                            >
+                                <img
+                                    src={logo}
+                                    alt={'logo'}
+                                    width={'10%'}
+                                    height={'auto'}
+                                />
+                                <img
+                                    src={autComputerEngineeringLogo}
+                                    alt={'aut-computer-engineering-logo'}
+                                    width={'10%'}
+                                    height={'auto'}
+                                />
+                                <img
+                                    src={tehranArtLogo}
+                                    alt={'tehran-art-logo'}
+                                    width={'10%'}
+                                    height={'auto'}
+                                />
+                            </Flex>
+                        </Col>
+                        <Col span={24} md={12}>
+                            <Flex
+                                align={"center"}
+                                justify={"center"}
+                                flex={1}
+                                gap={"small"}
+                            >
+                                <Button type={"text"} shape={"circle"} size={"large"} icon={<YoutubeFilled/>}/>
+                                <Button type={"text"} shape={"circle"} size={"large"} icon={<XOutlined/>}/>
+                                <Button type={"text"} shape={"circle"} size={"large"} icon={<InstagramOutlined/>}/>
+                                <Button type={"text"} shape={"circle"} size={"large"} icon={<FacebookFilled/>}/>
+                                <Button type={"text"} shape={"circle"} size={"large"} icon={<LinkedinFilled/>}/>
+                            </Flex>
+                        </Col>
+                    </Row>
+                </Flex>
+                <Divider>
+                    <Typography.Text type={"secondary"}>
+                        {t('app.footer.copyRight')}
+                    </Typography.Text>
+                </Divider>
+            </Layout.Footer>
+        </ConfigProvider>
     )
 }
--- src/components/introduction/GameCraftIntro.jsx
+++ src/components/introduction/GameCraftIntro.jsx
@@ -1,8 +1,9 @@
-import {Button, Col, Flex, Grid, Row, theme, Typography} from "antd";
+import {Button, Col, ConfigProvider, Flex, Grid, Row, theme, Typography} from "antd";
 import logo from '/src/assets/svg/dark-3d-bulb.svg'
 import {InstagramOutlined, XOutlined, YoutubeFilled} from "@ant-design/icons";
 import {useTranslation} from "react-i18next";
 import bubbleImg from '/src/assets/svg/bubble-purple.svg'
+import {darkTheme} from "../../config/theme/darkTheme.js";
 
 const {useToken} = theme
 const {useBreakpoint} = Grid;
@@ -13,109 +14,111 @@ export function GameCraftIntro({padding, backgroundColor}) {
     const screens = useBreakpoint();
 
     return (
-        <Flex
-            vertical
-            align={"center"}
-            justify={"center"}
-            style={{
-                width: '100%',
-                padding: padding,
-                backgroundColor: backgroundColor,
-            }}
-        >
-            <Row align={"middle"} justify={"space-around"} gutter={[16, 16]} style={{width: '100%'}}>
-                <Col span={24} lg={12}>
-                    <Flex
-                        vertical
-                        align={"start"}
-                        justify={"start"}
-                        style={{
-                            width: '100%',
-                            position: 'relative',
-                            zIndex: 10
-                        }}
-                        gap={"small"}
-                    >
-                        <Flex align={"center"} justify={"center"} style={{
-                            width: '100%',
-                            height: '100%',
-                            position: "absolute",
-                            zIndex: -1,
-                        }}>
-                            <img
-                                src={bubbleImg}
-                                alt={'bubble-image'}
-                                width={'100%'}
-                                height={'100%'}
-                                style={{
-                                    transform: 'scaleX(-1)',
-                                }}
-                            />
-                        </Flex>
-                        <Typography.Title level={1} style={{
-                            color: token.colorAction,
-                            fontWeight: 1000,
-                            fontSize: screens.lg ? '5rem' : screens.md ? '4rem' : '3rem',
-                            marginBottom: '1rem'
-                        }}>
-                            {t('app.intro.title')}
-                        </Typography.Title>
-                        <Typography.Title
-                            level={3}
+        <ConfigProvider theme={darkTheme}>
+            <Flex
+                vertical
+                align={"center"}
+                justify={"center"}
+                style={{
+                    width: '100%',
+                    padding: padding,
+                    backgroundColor: backgroundColor,
+                }}
+            >
+                <Row align={"middle"} justify={"space-around"} gutter={[16, 16]} style={{width: '100%'}}>
+                    <Col span={24} lg={12}>
+                        <Flex
+                            vertical
+                            align={"start"}
+                            justify={"start"}
                             style={{
-                                fontWeight: 900,
-                                margin: 0
+                                width: '100%',
+                                position: 'relative',
+                                zIndex: 10
                             }}
+                            gap={"small"}
                         >
-                            {t('app.intro.subtitle')}
-                        </Typography.Title>
-                        <Typography.Paragraph>
-                            {t('app.intro.description')}
-                        </Typography.Paragraph>
-                        <Flex align={"center"} justify={"start"} style={{width: '100%'}} gap={"small"} wrap>
-                            <Button
-                                type={"primary"}
-                                size={"large"}
-                                shape={"circle"}
-                                icon={<XOutlined style={{fontSize: '2rem'}}/>}
+                            <Flex align={"center"} justify={"center"} style={{
+                                width: '100%',
+                                height: '100%',
+                                position: "absolute",
+                                zIndex: -1,
+                            }}>
+                                <img
+                                    src={bubbleImg}
+                                    alt={'bubble-image'}
+                                    width={'100%'}
+                                    height={'100%'}
+                                    style={{
+                                        transform: 'scaleX(-1)',
+                                    }}
+                                />
+                            </Flex>
+                            <Typography.Title level={1} style={{
+                                color: token.colorAction,
+                                fontWeight: 1000,
+                                fontSize: screens.lg ? '5rem' : screens.md ? '4rem' : '3rem',
+                                marginBottom: '1rem'
+                            }}>
+                                {t('app.intro.title')}
+                            </Typography.Title>
+                            <Typography.Title
+                                level={3}
                                 style={{
-                                    width: '3.5rem',
-                                    height: '3.5rem',
-                                    background: token.colorAction
+                                    fontWeight: 900,
+                                    margin: 0
                                 }}
-                            />
-                            <Button
-                                type={"primary"}
-                                size={"large"}
-                                shape={"circle"}
-                                icon={<InstagramOutlined style={{fontSize: '2rem'}}/>}
-                                style={{
-                                    width: '4rem',
-                                    height: '4rem',
-                                    background: token.colorAction
-                                }}
-                            />
-                            <Button
-                                type={"primary"}
-                                size={"large"}
-                                shape={"circle"}
-                                icon={<YoutubeFilled style={{fontSize: '2rem'}}/>}
-                                style={{
-                                    width: '3.5rem',
-                                    height: '3.5rem',
-                                    background: token.colorAction
-                                }}
-                            />
+                            >
+                                {t('app.intro.subtitle')}
+                            </Typography.Title>
+                            <Typography.Paragraph>
+                                {t('app.intro.description')}
+                            </Typography.Paragraph>
+                            <Flex align={"center"} justify={"start"} style={{width: '100%'}} gap={"small"} wrap>
+                                <Button
+                                    type={"primary"}
+                                    size={"large"}
+                                    shape={"circle"}
+                                    icon={<XOutlined style={{fontSize: '2rem'}}/>}
+                                    style={{
+                                        width: '3.5rem',
+                                        height: '3.5rem',
+                                        background: token.colorAction
+                                    }}
+                                />
+                                <Button
+                                    type={"primary"}
+                                    size={"large"}
+                                    shape={"circle"}
+                                    icon={<InstagramOutlined style={{fontSize: '2rem'}}/>}
+                                    style={{
+                                        width: '4rem',
+                                        height: '4rem',
+                                        background: token.colorAction
+                                    }}
+                                />
+                                <Button
+                                    type={"primary"}
+                                    size={"large"}
+                                    shape={"circle"}
+                                    icon={<YoutubeFilled style={{fontSize: '2rem'}}/>}
+                                    style={{
+                                        width: '3.5rem',
+                                        height: '3.5rem',
+                                        background: token.colorAction
+                                    }}
+                                />
+                            </Flex>
                         </Flex>
-                    </Flex>
-                </Col>
+                    </Col>
 
-                <Col span={24} lg={12}>
-                    <Flex align={"center"} justify={"center"} style={{width: '100%'}}>
-                        <img src={logo} alt={'logo'} width={'50%'} height={'auto'}/>
-                    </Flex>
-                </Col>
-            </Row>
-        </Flex>
+                    <Col span={24} lg={12}>
+                        <Flex align={"center"} justify={"center"} style={{width: '100%'}}>
+                            <img src={logo} alt={'logo'} width={'50%'} height={'auto'}/>
+                        </Flex>
+                    </Col>
+                </Row>
+            </Flex>
+        </ConfigProvider>
     )
 }
